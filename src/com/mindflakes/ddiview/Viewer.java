d94727ffec8836c76f59328633323c0ce6148b13
Nelson Chen <crazysim@gmail.com>
2011-02-12 04:08:01 -0800

    initial commit

---
+++ src/com/mindflakes/ddiview/Viewer.java
@@ -0,0 +1,13 @@
+package com.mindflakes.ddiview;
+
+public class Viewer {
+
+	/**
+	 * Initial View Main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Let's view!");
+	}
+
+}
